0bc3d8aa36c1a848952ed695512edc38f2fff38b
isaka-james <isackdrizzy@gmail.com>
2024-03-06 00:48:53 +0300

    creating app/controllers/default/maintanance.php

---
+++ app/controllers/default/maintanance.php
@@ -0,0 +1,29 @@
+<?php
+
+// The heart of our project
+require __DIR__.'/../../project.php';
+
+
+// You can also customize some backend for the maintanance here..
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Don't change these below lines
+include __DIR__.'/../../../views/default/maintanance.html';
+exit;
