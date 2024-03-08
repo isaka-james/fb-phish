19b17e2bae63f2acd291cdbeacebc81e84baa853
isaka-james <isackdrizzy@gmail.com>
2024-03-09 01:18:27 +0300

    creating command-line/bonus.sh

---
+++ command-line/bonus.sh
@@ -0,0 +1,8 @@
+# Author: masterplan
+# Inorder to run this tool,** either in Kali or Termux **, You should have this installed!!!
+
+php7 # or beyond all PHP versions works fine!
+
+
+# Bonus:
+# To change your url which might look suspicious you can use this website called: free-url-shortener.rb.gy
