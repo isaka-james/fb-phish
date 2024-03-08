5e6e9075fb6f1bd6eafa10f1a0f89a045cff9277
isaka-james <isackdrizzy@gmail.com>
2024-03-09 01:16:09 +0300

    creating app/Helpers/project-helper.php

---
+++ app/Helpers/project-helper.php
@@ -0,0 +1,28 @@
+<?php
+
+/******************************************|
+ *  Don't you ever modify this file!
+ *  This is where the logic of project
+ *    is get executed!
+ * 
+ * 
+ ******************************************/
+
+include_once __DIR__.'/../project.php';
+
+if($PROJECT_STATUS==3){
+    require_once __DIR__.'/../controllers/default/maintanance.php';
+
+
+
+}
+
+
+
+
+
+
+// // Import the DataBase Class
+// include __DIR__.'/database-master.php';
+
+// // To use database now we have $database variable
