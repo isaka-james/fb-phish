e7298830cc3693d270f3fe0ebcd6e928702d80cd
isaka-james <isackdrizzy@gmail.com>
2024-03-09 01:08:54 +0300

    creating testing/test1.php

---
+++ testing/test1.php
@@ -0,0 +1,10 @@
+<?php
+/*****************************************************|
+ *   This is the file for testing your logics,
+ *   make sure you make the project as 'development',
+ *   otherwise you won't able to access this file.
+ * 
+ *****************************************************/
+
+
+echo "test page write whetever you like here!!";
