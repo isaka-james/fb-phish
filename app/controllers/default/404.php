35414f43a8e7160c7c0b4b3c1fbb09ddc628ee7c
isaka-james <isackdrizzy@gmail.com>
2024-03-09 01:10:07 +0300

    creating app/controllers/default/404.php

---
+++ app/controllers/default/404.php
@@ -0,0 +1,35 @@
+<?php
+
+// The heart of our project
+require __DIR__.'/../../project.php';
+
+
+//Add your logic here to implement to the page 'views/errors/404.html'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Don't change these below lines
+include __DIR__.'/../../../views/default/404.html';
+exit;
+
