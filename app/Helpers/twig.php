e252c3aa15888fe607b48670b0d5cff51c494c7a
Masterplan <76619967+isaka-james@users.noreply.github.com>
2024-03-09 01:04:14 +0300

    new version

--- app/Helpers/twig.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Include the Composer autoload file
-require_once __DIR__.'/../../vendor/autoload.php';
- // Create a Twig loader and environment
- $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../../views'.$twig_dir);
- $twig = new \Twig\Environment($loader);
